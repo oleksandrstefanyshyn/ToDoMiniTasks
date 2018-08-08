18df8dd49adf9287497423ca520707aa179f1de1
Oleksandr <o.stefanyshyn@globallogic.com>
2018-08-08 12:02:28 +0300

    Working project

--- app/src/main/java/com/globallogic/trainee/ostefanyshyn/todominitasks/MainActivity.java
+++ app/src/main/java/com/globallogic/trainee/ostefanyshyn/todominitasks/MainActivity.java
@@ -2,12 +2,30 @@ package com.globallogic.trainee.ostefanyshyn.todominitasks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mEditText;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.s_button);
+        mEditText = findViewById(R.id.e_text);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), mEditText.getText().toString(),
+                        Toast.LENGTH_SHORT ).show();
+            }
+        });
     }
 }
